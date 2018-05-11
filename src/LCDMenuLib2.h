c41af2dc7b54b6aeab92e6663a2749b04843f1cf
Jomelo <nilsfeld@gmail.com>
2018-05-11 08:34:04 +0200

    change version

--- src/LCDMenuLib2.h
+++ src/LCDMenuLib2.h
@@ -67,7 +67,7 @@
     // Do nothing change here
     // ####################### //
     // Version
-    #define _LCDML_VERSION                       "LCDML2 v1.2.5"
+    #define _LCDML_VERSION                       "LCDML2 v1.2.6"
 
     // Include Arduino ios
     #include "Arduino.h"
